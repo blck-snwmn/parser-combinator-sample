5b4711c1ee624e76661134e82c4ac18aef162f27
blck-snwmn <whitesnowmancreator@gmail.com>
2019-02-13 12:34:07 +0900

    fix remove warning

--- src/test/scala/parser/ParserSpec.scala
+++ src/test/scala/parser/ParserSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest._
 class ParserSpec extends WordSpec with Matchers {
   "parser" should {
     "return success" in {
-      Parser("").parse("aaa") shouldBe a[ParseSuccess[String]]
+      Parser("").parse("aaa") shouldBe a[ParseSuccess[_]]
     }
   }
 }
