f6b0884548da0c33c993a72a03e49b3e818ddff9
blck-snwmn <whitesnowmancreator@gmail.com>
2019-03-04 21:42:58 +0900

    clean  clear some test

--- src/test/scala/parser/ParserSpec.scala
+++ src/test/scala/parser/ParserSpec.scala
@@ -5,34 +5,30 @@ import org.scalatest._
 class ParserSpec extends WordSpec with Matchers {
   "parser" should {
     "return success" in {
-      Parser("a").parse("abb") shouldBe a[ParseSuccess[_]]
       Parser("a").parse("a") shouldBe
         ParseSuccess("a", "")
       Parser("a").parse("abb") shouldBe
         ParseSuccess("a", "bb")
     }
     "return false" in {
-      Parser("b").parse("aaa") shouldBe a[ParseFailure]
-      Parser("bbbb").parse("aaa") shouldBe a[ParseFailure]
+      Parser("b").parse("aaa") shouldBe
+        ParseFailure("parse error. expected:b")
       Parser("bbbb").parse("aaa") shouldBe
         ParseFailure("parse error. expected:bbbb")
     }
   }
   "many parser" should {
     "no match" in {
-      Parser("a").many.parse("b") shouldBe a[ParseSuccess[_]]
       Parser("a").many.parse("b") shouldBe
         ParseSuccess(List(), "b")
     }
 
     "one match" in {
-      Parser("a").many.parse("a") shouldBe a[ParseSuccess[_]]
       Parser("a").many.parse("a") shouldBe
         ParseSuccess(List("a"), "")
     }
 
     "more match" in {
-      Parser("a").many.parse("aa") shouldBe a[ParseSuccess[_]]
       Parser("a").many.parse("aa") shouldBe
         ParseSuccess(List("a", "a"), "")
       Parser("a").many.parse("aab") shouldBe
@@ -42,19 +38,18 @@ class ParserSpec extends WordSpec with Matchers {
 
   "or parser" should {
     "match first parser" in {
-      Parser("a").or(Parser("b")).parse("aa") shouldBe a[ParseSuccess[_]]
       Parser("a").or(Parser("b")).parse("aa") shouldBe
         ParseSuccess("a", "a")
     }
 
     "match second parser" in {
-      Parser("a").or(Parser("b")).parse("ba") shouldBe a[ParseSuccess[_]]
       Parser("a").or(Parser("b")).parse("ba") shouldBe
         ParseSuccess("b", "a")
     }
 
     "no match" in {
-      Parser("a").or(Parser("b")).parse("cab") shouldBe a[ParseFailure]
+      Parser("a").or(Parser("b")).parse("cab") shouldBe
+        ParseFailure("parse error. expected:b")
     }
   }
 
