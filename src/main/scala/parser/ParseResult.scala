7d1d73c22469bc51c432570ae43b4f7988c1c565
blck-snwmn <whitesnowmancreator@gmail.com>
2019-04-13 15:24:33 +0900

    clean remove unnecessary parentheses

--- src/main/scala/parser/ParseResult.scala
+++ src/main/scala/parser/ParseResult.scala
@@ -1,26 +1,20 @@
 package parser
 
 sealed abstract class ParseResult[+T] {
-  def map[U](f: T => U): ParseResult[U] = {
-    this match {
-      case ParseSuccess(r, n) =>
-        ParseSuccess(f(r), n)
-      case _ => this.asInstanceOf[ParseResult[U]]
-    }
+  def map[U](f: T => U): ParseResult[U] = this match {
+    case ParseSuccess(r, n) =>
+      ParseSuccess(f(r), n)
+    case _ => this.asInstanceOf[ParseResult[U]]
   }
 
-  def flatMap[U](f: (T, String) => ParseResult[U]): ParseResult[U] = {
-    this match {
-      case ParseSuccess(r, n) => f(r, n)
-      case _ => this.asInstanceOf[ParseResult[U]]
-    }
+  def flatMap[U](f: (T, String) => ParseResult[U]): ParseResult[U] = this match {
+    case ParseSuccess(r, n) => f(r, n)
+    case _ => this.asInstanceOf[ParseResult[U]]
   }
 
-  def fold[U](fs: (T, String) => U, ff: String => U): U = {
-    this match {
-      case ParseSuccess(r, n) => fs(r, n)
-      case ParseFailure(m) => ff(m)
-    }
+  def fold[U](fs: (T, String) => U, ff: String => U): U = this match {
+    case ParseSuccess(r, n) => fs(r, n)
+    case ParseFailure(m) => ff(m)
   }
 }
 
